cb2d679ed609d6f1e71962c8a2bba1db76e1e5f4
Mark Politiko <politikomark@yandex.ru>
2023-03-02 14:40:17 +0300

    First commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable consistent-return */
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorizedErr');
